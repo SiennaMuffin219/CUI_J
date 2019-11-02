d52ceba3d7d09bc36f2aeeb7f1656acc3dc9dc6f
Olivier Pautrat <olivier.pautrat@laposte.net>
2019-11-02 19:12:29 +0100

    Add project files.

---
+++ CUI_J/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CUI_J
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
